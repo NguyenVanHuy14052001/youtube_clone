6d4fd2dcc7f86451abeff9c7b1a6007aff9dbbc5
HuyNguyen.DevPlus <huy.nguyen_dp05@devplus.edu.vn>
2023-03-08 15:36:58 +0700

    fix selected category name sidebar

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,16 +1,23 @@
 import { useState, useEffect } from 'react';
 import { Box, Stack, Typography } from '@mui/material';
-import Sidebar from './Sidebar';
-
-import Videos from './Videos';
+import { Sidebar, Videos} from './';
+import { fetchFromAPI } from '../utils/fetchFromAPI';
 const Feed = () => {
+  const [selectedCategory,
+  setSelectedCategory] = useState('New');
+  useEffect(() =>{
+    fetchFromAPI(`search?part=snippet&q=${selectedCategory}`);
+  }, [selectedCategory]);
   return (
     <Stack sx={{ flexDirection: { sx:
       "column", md:"row" } }}>
         <Box sx={{ height: { sx: 'auto', md:'92vh'}, 
       borderRight:'1px solid #3d3d3d', px: { sx: 0,
        md: 2 }}}>
-          <Sidebar />
+          <Sidebar 
+            selectedCategory={selectedCategory}
+            setSelectedCategory = {setSelectedCategory}
+          />
           <Typography className="copyright"
           variant="body2" sx={{ mt: 1.5,
           color: '#fff' }}>
@@ -26,7 +33,7 @@ const Feed = () => {
           }}>
             New <span style={{ color: '#F31503' }}>Videos</span>
           </Typography>
-          <Videos />
+          <Videos videos={[]}/>
         </Box>
     </Stack>
   )
